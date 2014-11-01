bbbcb48ceedea9f1e174e4660e9731f66636c255
Marques Johansson <marques@displague.com>
2014-10-31 22:16:20 -0400

    preserve the history longer

--- .bashrc.d/02_history.sh
+++ .bashrc.d/02_history.sh
@@ -1,11 +1,13 @@
 # don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
-HISTCONTROL=ignoreboth
+HISTCONTROL=ignoreboth:erasedups
 
 # append to the history file, don't overwrite it
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=1000
-HISTFILESIZE=2000
+HISTSIZE=100000
+HISTFILESIZE=200000
 
+# Save and reload the history after each command finishes
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
