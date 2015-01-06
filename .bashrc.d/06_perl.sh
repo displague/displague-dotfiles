e0ee06203895bef6860d07669cf695d155251625
Marques Johansson <marques@displague.com>
2015-01-05 21:42:52 -0500

    add usr/lib/perl5 to perl5lib so linode-cli works

--- .bashrc.d/06_perl.sh
+++ .bashrc.d/06_perl.sh
@@ -1,3 +1,4 @@
+PERL5LIB="/usr/lib/perl5"
 PERL5LIB="$HOME/perl5/lib/perl5${PERL5LIB+:}${PERL5LIB}"; export PERL5LIB;
 PERL_LOCAL_LIB_ROOT="$HOME/perl5${PERL_LOCAL_LIB_ROOT+:}${PERL_LOCAL_LIB_ROOT}"; export PERL_LOCAL_LIB_ROOT;
 PERL_MB_OPT="--install_base \"$HOME/perl5\""; export PERL_MB_OPT;
