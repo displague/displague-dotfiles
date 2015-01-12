a157da56f2d700b59e64f67a0dbe26ac3f3b7c6a
Marques Johansson <marques@displague.com>
2015-01-11 23:18:47 -0500

    remove unused bashrc block about globstar

--- .bashrc.d/07_less.sh
+++ .bashrc.d/07_less.sh
@@ -1,6 +1,2 @@
-# If set, the pattern "**" used in a pathname expansion context will
-# match all files and zero or more directories and subdirectories.
-#shopt -s globstar
-
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
