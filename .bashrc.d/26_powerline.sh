1a259b599950e369737679d4699dc6ab9b09d89e
Marques Johansson <marques@displague.com>
2015-02-25 21:09:18 -0500

    fix ~/src path to powerline

--- .bashrc.d/26_powerline.sh
+++ .bashrc.d/26_powerline.sh
@@ -1,6 +1,6 @@
 for tmuxdir in \
-$HOME/src/powerline/build/lib/powerline/binding/tmux/ \
+$HOME/src/powerline/build/lib/powerline/bindings/tmux/ \
 /usr/share/powerline/bindings/tmux/ \
-; do 
-  [ -d "$tmuxdir" ] && export TMUX_POWERLINE_CONF="$tmuxdir"; 
+; do
+  [ -d "$tmuxdir" ] && export TMUX_POWERLINE_CONF="$tmuxdir";
 done
