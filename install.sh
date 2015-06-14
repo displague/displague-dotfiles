fd18850d57e6d0709500470312a9473638ab2ead
Marques Johansson <marques@displague.com>
2015-06-14 18:34:12 -0400

    long line crunch

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ CHECKOUT=~/.local/share/dotfiles
 
 for s in $CHECKOUT/.vim $CHECKOUT/.vimrc; do ln -s $s ~; done
 
+url=https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim # https://tpo.pe/pathogen.vim
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-	curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim # https://tpo.pe/pathogen.vim
+	curl -LSso ~/.vim/autoload/pathogen.vim "$url"
