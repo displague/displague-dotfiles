d651cd18e5509920bd4c4fdc7db9987722088f7c
Marques Johansson <marques@displague.com>
2015-06-17 23:41:03 -0400

    terrible spacing

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 CHECKOUT=~/.local/share/dotfiles
 
-for s in $CHECKOUT/.vim $CHECKOUT/.vimrc; do ln -s $s ~; done
+for s in $CHECKOUT/.vim $CHECKOUT/.vimrc; do
+  ln -s $s ~;
+done
 
-url=https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim # https://tpo.pe/pathogen.vim
+url=i"https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim" # https://tpo.pe/pathogen.vim
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 	curl -LSso ~/.vim/autoload/pathogen.vim "$url"
