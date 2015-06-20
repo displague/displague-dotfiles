6125ea00895601d1895452aa6ea5e1bea8400cd5
Marques Johansson <marques@displague.com>
2015-06-19 23:32:09 -0400

    remove stray i

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ for s in $CHECKOUT/.vim $CHECKOUT/.vimrc; do
   ln -s $s ~;
 done
 
-url=i"https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim" # https://tpo.pe/pathogen.vim
+url="https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim" # https://tpo.pe/pathogen.vim
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 	curl -LSso ~/.vim/autoload/pathogen.vim "$url"
