8961a72c1921f518a8089d13aafa1c5294bfb125
Marques Johansson <marques@displague.com>
2015-06-21 23:05:16 -0400

    minor comment relocation

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ for s in $CHECKOUT/.vim $CHECKOUT/.vimrc; do
   ln -s $s ~;
 done
 
-url="https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim" # https://tpo.pe/pathogen.vim
+# https://tpo.pe/pathogen.vim
+url="https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim"
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 	curl -LSso ~/.vim/autoload/pathogen.vim "$url"
