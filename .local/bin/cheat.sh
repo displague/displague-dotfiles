73e474a26fc05cdc3b046aaff10944f5765c8387
Marques Johansson <marques@displague.com>
2015-08-28 23:12:57 -0400

    build youcompleteme on update

--- .local/bin/cheat.sh
+++ .local/bin/cheat.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # GitHub Streak Keeper / Helper
 # - Because vim submodule updates count
 (
@@ -6,8 +6,11 @@
     git submodule update --init --recursive;
     for dir in *; do (
       cd $dir;
-      git pull --rebase origin master;
-      git submodule update --init --recursive
+      git pull --rebase origin master \
+	| grep -q -v 'is up to date' \
+	&& git submodule update --init --recursive \
+	&& [ "z$(basename $PWD)" = "zYouCompleteMe" ] \
+	&& ./install.py --gocode-completer --clang-completer
       cd ..;
       git commit -m "updated .vim bundle $dir" $dir > /dev/null && git push
     ); done
