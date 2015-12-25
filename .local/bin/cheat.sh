73684704536284d8ad30ccd7d4eeb70863a13560
Marques Johansson <marques@displague.com>
2015-12-25 18:41:23 -0500

    use && instead of set -x

--- .local/bin/cheat.sh
+++ .local/bin/cheat.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-set -x -e
+set -e
 # GitHub Streak Keeper / Helper
 # - Because vim submodule updates count
 (
   cd ~/.vim/bundle/ && (
-    git checkout master
-    git pull --rebase origin master
-    git submodule update --init --recursive;
-    for dir in *; do (
-      cd $dir;
-      git pull --rebase origin master \
-	| grep -q -v 'is up to date' \
-	&& git submodule update --init --recursive \
-	&& [ "z$(basename $PWD)" = "zYouCompleteMe" ] \
-	&& ./install.py --gocode-completer --clang-completer
-      cd ..;
-      git commit -m "updated .vim bundle $dir" $dir > /dev/null && git push
+    git checkout master \
+      && git pull --rebase origin master \
+      && git submodule update --init --recursive \
+    && for dir in *; do (
+         cd $dir;
+         git pull --rebase origin master \
+	   | grep -q -v 'is up to date' \
+	   && git submodule update --init --recursive \
+	   && [ "z$(basename $PWD)" = "zYouCompleteMe" ] \
+	   && ./install.py --gocode-completer --clang-completer
+         cd ..;
+         git commit -m "updated .vim bundle $dir" $dir > /dev/null && git push
     ); done
   )
 )
