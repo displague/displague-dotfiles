2cf8d0f4d6352f38831484f9d27f9dd5888b000a
Marques Johansson <marques@displague.com>
2016-03-11 21:50:27 -0500

    dont look for hss more than once

--- .bashrc.d/21_hss.sh
+++ .bashrc.d/21_hss.sh
@@ -1,4 +1,6 @@
-command -v hss > /dev/null && alias pcs='scp -S hss'
-command -v hss > /dev/null && alias ptfs='sftp -S hss'
-command -v hss > /dev/null && alias cnysr='rsync -e hss'
+if command -v hss > /dev/null; then
+    alias pcs='scp -S hss'
+    alias ptfs='sftp -S hss'
+    alias cnysr='rsync -e hss'
+fi
 
