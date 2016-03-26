d7b5b0b6ee06b1120f5c43700a85102089d61d4a
Marques Johansson <marques@displague.com>
2016-03-25 22:57:07 -0400

    remove sublime aliases

--- .bashrc.d/99_custom.sh
+++ .bashrc.d/99_custom.sh
@@ -1,5 +1,3 @@
-[ -f '/opt/Sublime\ Text\ 2/sublime_text' ] && alias sl='/opt/Sublime\ Text\ 2/sublime_text'
-[ -f '/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl' ] && alias sl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
 command -v hub > /dev/null && alias git=hub
 
 function server() {
