393e90614a13ac143a2f10db1419e6fe8aa7e0e2
Marques Johansson <marques@packet.com>
2020-07-31 08:28:26 -0400

    pick-up brew and xcode autocompletions

--- .bashrc.d/25_completions.sh
+++ .bashrc.d/25_completions.sh
@@ -2,9 +2,22 @@
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
 if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-  fi
+  # First
+  for path in /usr/local/Cellar/bash-completion/*/etc/bash_completion \
+    /usr/share/bash-completion/bash_completion \
+    /etc/bash_completion; do
+    if [ -f $path ]; then
+      . $path
+      break
+    fi
+  done
+
+  # All
+  for path in /Applications/Xcode.app//Contents/Developer/usr/share/git-core/git-completion.bash \
+    ; do
+    if [ -f $path ]; then
+      . $path
+      break
+    fi
+  done
 fi
