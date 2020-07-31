e2c652f267c2204a55aa24e2cc71ce21466a03d5
Marques Johansson <marques@packet.com>
2020-07-31 08:28:59 -0400

    include packet completions

--- .bashrc.d/99_custom.sh
+++ .bashrc.d/99_custom.sh
@@ -1,5 +1,7 @@
 command -v hub > /dev/null && alias git=hub
 
+which packet > /dev/null && eval "$(packet completion bash)"
+
 function server() {
 	sensible-browser "http://localhost:${1}" && python -m SimpleHTTPServer $1
 }
