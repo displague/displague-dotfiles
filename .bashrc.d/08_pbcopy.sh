9a3cac5adaabc53706a7bb6125cdc00f32f445c9
Marques Johansson <marques@packet.com>
2020-07-31 08:27:48 -0400

    only alias pbcopy/pbpaste when they dont exist (dont break macs)

--- .bashrc.d/08_pbcopy.sh
+++ .bashrc.d/08_pbcopy.sh
@@ -1,3 +1,4 @@
-alias pbcopy='xclip -selection clipboard'
-alias pbpaste='xclip -selection clipboard -o'
-
+if [ ! -x /usr/bin/pbcopy ]; then
+	alias pbcopy='xclip -selection clipboard'
+	alias pbpaste='xclip -selection clipboard -o'
+fi
